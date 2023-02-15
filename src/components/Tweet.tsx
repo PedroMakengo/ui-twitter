8b39b1fd5a133b2b2ff964ade9f795bc75c2d83d
PedroMakengo <pedromakengomiguel@gmail.com>
2023-02-15 13:47:38 +0100

    Trabalhando na parte do nav

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,8 +1,19 @@
-export function Tweet() {
+interface TweetProps {
+  user: string;
+  children: string;
+  likes?: number;
+}
+
+const tweetStyles = {
+  borderRadius: 8,
+};
+
+export function Tweet({ user, children, likes }: TweetProps) {
   return (
-    <div>
-      Tweet
-      <button>Like</button>
+    <div className="tweet" style={tweetStyles}>
+      <h2>{user}</h2>
+      <p>{children}</p>
+      <button>Like {!likes ? '0' : likes}</button>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Tweet } from './components/Tweet';
+
+import logoTwitter from './assets/logo-twitter.svg';
+
+import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Tweet />
+    <div className="layout">
+      <aside className="sidebar">
+        <img src={logoTwitter} alt="" className="logo" />
+
+        <nav className="main-navigation">
+          <a href="">Home</a>
+          <a href="">Explore</a>
+          <a href="">Notifications</a>
+          <a href="">Messages</a>
+          <a href="">Lists</a>
+          <a href="">Profile</a>
+          <a href="">More</a>
+        </nav>
+        <button className="new-tweet" type="button">
+          Tweet
+        </button>
+      </aside>
+      <div className="content">Content</div>
+    </div>
   </React.StrictMode>
 );
